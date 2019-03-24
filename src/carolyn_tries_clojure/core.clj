fa4b3465ecf8e6c62a90d3203a62fc7cf41cbba3
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2019-03-24 01:39:16 +0000

    Introduce minutes

--- src/carolyn_tries_clojure/core.clj
+++ src/carolyn_tries_clojure/core.clj
@@ -7,12 +7,37 @@
 
 ;(print (str hello-world " and good evening"))    ;concatenate 2 strings
 
-(defn format-time
+(defn format-seconds
   "Given a number of seconds, display as nice string"
   [seconds]
   (cond
-    (= seconds 0) "none"
     (= seconds 1) (str seconds " second")
     :else (str seconds " seconds")
     )
+  )
+
+
+(defn format-minutes
+  "Given a number of minutes, display as nice string"
+  [minutes]
+  (cond
+    (= minutes 1) (str minutes " minute")
+    :else (str minutes " minutes")
+    )
+  )
+
+(defn format-time
+  "Format a time"
+  [input-seconds]
+
+  (let [minutes (quot input-seconds 60) seconds (rem input-seconds 60)]
+
+    (cond
+      (= input-seconds 0) "none"
+      (<= minutes 0) (format-seconds seconds)
+      :else  (str (format-minutes minutes) " and " (format-seconds seconds))
+      )
+    )
+
+
   )
--- test/carolyn_tries_clojure/core_test.clj
+++ test/carolyn_tries_clojure/core_test.clj
@@ -9,6 +9,10 @@
     (is (= (format-time 1) "1 second")))
   (testing "should print out 0 seconds as none"
     (is (= (format-time 0) "none")))
+  (testing "should split into minutes if > 59 seconds"
+    (is (= (format-time 123) "2 minutes and 3 seconds")))
+  (testing "should show 1 minute as singular"
+    (is (= (format-time 70) "1 minute and 10 seconds")))
   )
 
 (run-tests 'carolyn-tries-clojure.core-test)
