d8cbd915ea76c66c8007f349b8c9cd190f7d426c
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2019-03-24 14:31:23 +0000

    Introduce days

--- src/carolyn_tries_clojure/core.clj
+++ src/carolyn_tries_clojure/core.clj
@@ -27,14 +27,33 @@
   (format-unit-with-measure hours "hour")
   )
 
+(defn format-days
+  [days]
+  (format-unit-with-measure days "day")
+  )
+
+(def seconds-in-minute 60)
+(def minutes-in-hour 60)
+(def hours-in-day 24)
+(def days-in-year 365)
+
+(def seconds-in-hour (* seconds-in-minute minutes-in-hour))
+(def seconds-in-day (* seconds-in-hour hours-in-day))
+(def seconds-in-year (* seconds-in-day days-in-year))
+
+(def minutes-in-day (* minutes-in-hour hours-in-day))
+(def minutes-in-year (* minutes-in-day days-in-year))
+
 (defn split-by-time-unit
   "Given seconds, split into an array of hours, minutes, seconds etc"
   [input-seconds]
-  (let [hours (quot input-seconds 3600)
-        minutes (- (quot input-seconds 60) (* hours 60))
+  (let [
+        days (quot input-seconds seconds-in-day)
+        hours (- (quot input-seconds seconds-in-hour) (* days hours-in-day))
+        minutes (- (quot input-seconds seconds-in-minute) (+ (* hours minutes-in-hour) (* days minutes-in-day) ) )
         seconds (rem input-seconds 60)
         ]
-    (remove clojure.string/blank? [(format-hours hours) (format-minutes minutes) (format-seconds seconds)])
+    (remove clojure.string/blank? [ (format-days days) (format-hours hours) (format-minutes minutes) (format-seconds seconds)])
     )
   )
 
--- test/carolyn_tries_clojure/core_test.clj
+++ test/carolyn_tries_clojure/core_test.clj
@@ -9,18 +9,27 @@
     (is (= (format-time 1) "1 second")))
   (testing "should print out 0 seconds as none"
     (is (= (format-time 0) "none")))
+
   (testing "should split into minutes if > 59 seconds"
     (is (= (format-time 123) "2 minutes and 3 seconds")))
   (testing "should show 1 minute as singular"
     (is (= (format-time 70) "1 minute and 10 seconds")))
   (testing "should omit seconds if a whole number of minutes"
     (is (= (format-time 120) "2 minutes")))
+
   (testing "should split into hours if > 59 minutes"
     (is (= (format-time 3662) "1 hour, 1 minute and 2 seconds")))
   (testing "should omit minutes and seconds if a whole number of hours"
     (is (= (format-time 3600) "1 hour")))
-  (testing "should omit minutes if a time is hours and seconds only"
+  (testing "should omit minutes if hours and seconds only"
     (is (= (format-time 3604) "1 hour and 4 seconds")))
+  (testing "should print out multiple hours as plural"
+    (is (= (format-time 7200) "2 hours")))
+
+
+  (testing "should split into days if > 23 hours"
+    (is (= (format-time 90062) "1 day, 1 hour, 1 minute and 2 seconds")))
+
   )
 
 (run-tests 'carolyn-tries-clojure.core-test)
